14a6e92a5619c29d7a423dc75a88a25af381ba39
chopinrlz <chopinrlz@gmail.com>
2023-10-29 18:06:50 -0400

    Master and duplicate tracking

--- Deduplicate-Folders.ps1
+++ Deduplicate-Folders.ps1
@@ -1,51 +1,152 @@
+<#
+    .SYNOPSIS
+    Detects duplicate files between two folder trees.
+    .PARAMETER Master
+    The master folder tree to scan first and treat as the primary source.
+    .PARAMETER Duplicate
+    The duplicates folder tree to scan second and treat as the duplicates location.
+    .PARAMETER HardLimit
+    An optional, arbitrary hard limit of files to process before stopping, if you want to limit the processing time.
+    .PARAMETER TreatZeroAsDuplicate
+    An optional switch to include zero-length files in the duplicates report.
+    .PARAMETER TrackMasterDuplicates
+    An optional switch to track duplicate files in the master directory.
+#>
 param(
     [Parameter(Mandatory=$true)]
-    [string]$Master,
+    [string]
+    $Master,
     [Parameter(Mandatory=$true)]
-    [string]$Duplicate,
+    [string]
+    $Duplicate,
     [Parameter(Mandatory=$false)]
     [ValidateRange(1,999999999)]
-    [int]$HardLimit = [Int32]::MaxValue,
+    [int]
+    $HardLimit = [Int32]::MaxValue,
     [Parameter(Mandatory=$false)]
-    [switch]$TreatZeroAsDuplicate
+    [switch]
+    $TreatZeroAsDuplicate,
+    [switch]
+    $TrackMasterDuplicates
 )
+
+# Assertions
 if( -not (Test-Path $Master) ) {
-    Write-Error "Path specified as Master $Master does not exist or cannot be accessed"
-    exit
+    throw "Path specified as Master $Master does not exist or cannot be accessed"
 }
 if( -not (Test-Path $Duplicate) ) {
-    Write-Error "Path specified as Duplicate $Duplicate does not exist or cannot be accessed"
-    exit
+    throw "Path specified as Duplicate $Duplicate does not exist or cannot be accessed"
 }
-$master = @{}
+if( $Master -eq $Duplicate ) {
+    throw "You cannot specify the same path for both Master and Duplicate"
+}
+
+# Setup variables
+$table = @{}
 $duplicates = @()
-$children = Get-ChildItem -Path $Master -Recurse -File
-$total = $children.Length
+$masterFiles = Get-ChildItem -Path $Master -Recurse -File
+$total = $masterFiles.Length
 $counter = 0
-foreach( $child in $children ) {
-    if( $child.Length -gt 0 ) {
-        $hash = (Get-FileHash ($child.FullName)).
+$hash = $null
+
+# Process the master tree first
+foreach( $file in $masterFiles ) {
+    if( $file.Length -gt 0 ) {
+        try {
+            $hash = (Get-FileHash ($file.FullName)).Hash
+        } catch {
+            $hash = $null
+        }
         if( $hash ) {
-            if( $master.ContainsKey( $hash ) ) {
-                $duplicates += $child
+            if( $table.ContainsKey( $hash ) ) {
+                if( $TrackMasterDuplicates ) {
+                    $originalFile = $table[$hash]
+                    $dupe = [PSCustomObject]@{
+                        Original = $originalFile.FullName
+                        Duplicate = $file.FullName
+                        FileSize = $file.Length
+                        Hash = $hash
+                    }
+                    $duplicates += $dupe
+                }
             } else {
-                $master.Add( $hash, $child )
+                $table.Add( $hash, $file )
             }
         } else {
-
+            Write-Warning "File $($file.FullName) could not be read"
         }
     } else {
-        if( $TreatZeroAsDuplicate ) {
-            $duplicates += $child
+        if( $TreatZeroAsDuplicate -and $TrackMasterDuplicates ) {
+            $dupe = [PSCustomObject]@{
+                Original = $file.FullName
+                Duplicate = $file.FullName
+                FileSize = $file.Length
+                Hash = "zero-length file"
+            }
+            $duplicates += $dupe
         }
     }
     $counter++
     if( $counter -gt $HardLimit ) {
         break
     }
-    Write-Progress -Activity "Searching for Duplicates" -Status "In Progress ($counter of $total)" -PercentComplete ($counter / $total * 99.9)
+    Write-Progress -Activity "Building Master Tree" -Status "In Progress ($counter of $total)" -PercentComplete ($counter / $total * 99.9)
 }
+
+# Fetch the duplicate tree files and reset variables
+$duplicateFiles = Get-ChildItem -Path $Duplicate -Recurse -File
+$total = $duplicateFiles.Length
+$counter = 0
+$hash = $null
+
+# Process the duplicates file tree second
+foreach( $file in $duplicateFiles ) {
+    if( $file.Length -gt 0 ) {
+        try {
+            $hash = (Get-FileHash ($file.FullName)).Hash
+        } catch {
+            $hash = $null
+        }
+        if( $hash ) {
+            if( $table.ContainsKey( $hash ) ) {
+                $originalFile = $table[$hash]
+                $dupe = [PSCustomObject]@{
+                    Original = $originalFile.FullName
+                    Duplicate = $file.FullName
+                    FileSize = $file.Length
+                    Hash = $hash
+                }
+                $duplicates += $dupe
+            } else {
+                $table.Add( $hash, $file )
+            }
+        } else {
+            Write-Warning "File $($file.FullName) could not be read"
+        }
+    } else {
+        if( $TreatZeroAsDuplicate ) {
+            $dupe = [PSCustomObject]@{
+                Original = $file.FullName
+                Duplicate = $file.FullName
+                FileSize = $file.Length
+                Hash = "zero-length file"
+            }
+            $duplicates += $dupe
+        }
+    }
+    $counter++
+    if( $counter -gt $HardLimit ) {
+        break
+    }
+    Write-Progress -Activity "Processing Duplicates Tree" -Status "In Progress ($counter of $total)" -PercentComplete ($counter / $total * 99.9)
+}
+
+# Report the duplicates to a file
 if( $duplicates ) {
+    Write-Host "Exporting duplicates report"
+    if( Test-Path "duplicates.csv" ) {
+        Remove-Item "duplicates.csv"
+    }
     $duplicates | ForEach-Object {
         Export-Csv -InputObject $_ -Path "duplicates.csv" -Append
     }
