c59b679f4bcd7a6707522589f7b99942707e2b7d
Christian Holslin <cholslin@shwatech.com>
2025-02-20 15:09:33 -0500

    Started import script for staff metadata

---
+++ Import-StaffMetadata.ps1
@@ -0,0 +1,38 @@
+param(
+    [Parameter(Mandatory)]
+    [string]
+    $Path,
+    [Parameter(Mandatory)]
+    [string]
+    $Tenant,
+    [Parameter(Mandatory)]
+    [string]
+    $Certificate
+)
+
+# Validate and fetch the staff metadata
+if( Test-Path $Path ) {
+    # OK
+} else {
+    throw "Staff member CSV file $Path does not exist"
+}
+$staff = Import-Csv $Path
+
+# Validate the tenant
+if( $Tenant -contains '.' ) {
+    throw "Tenant name must contain only the root hostname"
+}
+
+# Validate and fetch the certificate metadata
+if( Test-Path $Certificate ) {
+    # OK
+} else {
+    throw "Certificate metadata file $Certficiate does not exist"
+}
+. (Join-Path -Path $PSScriptRoot -ChildPath 'Read-EntraAppCert.ps1')
+$cert = Read-EntraAppCert -Path $Certificate
+if( $cert.CertThumb ) {
+    # OK
+} else {
+    throw "Certificate metadata file $Certificate does not include a thumbprint"
+}
